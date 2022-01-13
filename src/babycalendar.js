9cc33ecd2f18c252045de753901f0ee6a1d294a6
Adam Presley <adam@adampresley.com>
2022-01-13 16:37:41 -0600

    All the basics work!

--- src/babycalendar.js
+++ src/babycalendar.js
@@ -2,18 +2,53 @@ export class BabyCalendar extends HTMLElement {
 	constructor() {
 		super();
 
-		this._header = null;
-		this._bodyID = "";
-		this._body = null;
+		this._daysOfTheWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+		this._months = [
+			"January",
+			"February",
+			"March",
+			"April",
+			"May",
+			"June",
+			"July",
+			"August",
+			"September",
+			"October",
+			"November",
+			"December",
+		];
+
+		this._navigationMonthID = "";
+		this._navigationMonthEl = null;
+		this._headerEl = null;
+		this._bodyElID = "";
+		this._bodyEl = null;
 
 		let d = new Date();
 
+		let navigationAttr = this.getAttribute("shownavigation");
+		this._showNavigation =
+			navigationAttr === "true" ||
+			navigationAttr === "1" ||
+			navigationAttr === "yes"
+				? true
+				: false;
 		this._month = parseInt(this.getAttribute("month")) || d.getMonth();
 		this._year = parseInt(this.getAttribute("year")) || d.getFullYear();
 	}
 
 	connectedCallback() {
-		this._render(this._month, this._year);
+		this._render();
+	}
+
+	get showNavigation() {
+		return this._showNavigation;
+	}
+
+	set showNavigation(value) {
+		this._showNavigation = value;
+		this.setAttribute("shownavigation", value);
+		this.redraw();
 	}
 
 	get month() {
@@ -37,50 +72,113 @@ export class BabyCalendar extends HTMLElement {
 	/*****************************************************************************
 	 * Public methods
 	 ****************************************************************************/
+	nextMonth() {
+		this._onRightClick();
+	}
+
+	previousMonth() {
+		this._onLeftClick();
+	}
+
 	redraw() {
-		this._drawBody();
+		if (this._showNavigation) {
+			this._updateNavigation();
+		}
+
+		this._updateBody();
+	}
+
+	today() {
+		let d = new Date();
+		this._month = d.getMonth();
+		this._year = d.getFullYear();
+
+		this.redraw();
+		this._dispatchChangeMonth();
 	}
 
 	/*****************************************************************************
 	 * Private methods
 	 ****************************************************************************/
+	_dispatchChangeMonth() {
+		this.dispatchEvent(
+			new CustomEvent("babycalendar-changemonth", {
+				detail: {
+					month: this._month,
+					year: this._year,
+				},
+			})
+		);
+	}
+
 	_generateRandomID(prefix) {
 		return `${prefix}-${Date.now()}`;
 	}
 
-	_getFirstDayOfMonth(month, year) {
-		let d = new Date(year, month);
+	_getFirstDayOfMonth() {
+		let d = new Date(this._year, this._month);
 		let result = new Date(d.getFullYear(), d.getMonth(), 1);
 		return result;
 	}
 
-	_getLastDayOfMonth(month, year) {
-		let d = new Date(year, month);
+	_getLastDayOfMonth() {
+		let d = new Date(this._year, this._month);
 		let result = new Date(d.getFullYear(), d.getMonth() + 1, 0);
 		return result;
 	}
 
-	_render(month, year) {
+	_onLeftClick() {
+		this._month--;
+
+		if (this._month < 0) {
+			this._month = 11;
+			this._year--;
+		}
+
+		this.redraw();
+		this._dispatchChangeMonth();
+	}
+
+	_onRightClick() {
+		this._month++;
+
+		if (this._month > 11) {
+			this._month = 0;
+			this._year++;
+		}
+
+		this.redraw();
+		this._dispatchChangeMonth();
+	}
+
+	_render() {
 		this.classList.add("babycalendar");
-		this._header = this._renderHeader();
-		this._body = this._renderBody(month, year);
 
-		this._bodyID = this._generateRandomID("body");
-		this._body.id = this._bodyID;
+		if (this._showNavigation) {
+			this._renderNavigation();
+		}
 
-		this.insertAdjacentElement("beforeend", this._header);
-		this.insertAdjacentElement("beforeend", this._body);
+		this._renderHeader();
+		this._renderBody();
+	}
+
+	_renderNavigation() {
+		let div = this._drawNavigation();
+		this.insertAdjacentElement("beforeend", div);
+
+		this._updateNavigation();
 	}
 
 	_renderHeader() {
 		let div = document.createElement("div");
 		div.classList.add("header-row");
 
-		["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].forEach((dayName) => {
+		this._daysOfTheWeek.forEach((dayName) => {
 			this._renderHeaderDayCell(div, dayName);
 		});
 
-		return div;
+		this._headerEl = div;
+		this.insertAdjacentElement("beforeend", this._headerEl);
 	}
 
 	_renderHeaderDayCell(parentEl, dayName) {
@@ -91,15 +189,46 @@ export class BabyCalendar extends HTMLElement {
 		parentEl.insertAdjacentElement("beforeend", div);
 	}
 
-	_renderBody(month, year) {
+	_renderBody() {
+		this._bodyEl = this._drawBody();
+		this._bodyElID = this._generateRandomID("body");
+		this._bodyEl.id = this._bodyElID;
+		this.insertAdjacentElement("beforeend", this._bodyEl);
+	}
+
+	_drawNavigation() {
+		let div = document.createElement("div");
+		div.classList.add("navigation");
+
+		let left = document.createElement("button");
+		left.classList.add("navigation-button", "left");
+		left.addEventListener("click", this._onLeftClick.bind(this));
+
+		let right = document.createElement("button");
+		right.classList.add("navigation-button", "right");
+		right.addEventListener("click", this._onRightClick.bind(this));
+
+		this._navigationMonthID = this._generateRandomID("navigationlabel");
+		this._navigationMonthEl = document.createElement("span");
+		this._navigationMonthEl.id = this._navigationMonthID;
+		this._navigationMonthEl.classList.add("navigation-label");
+
+		div.insertAdjacentElement("beforeend", left);
+		div.insertAdjacentElement("beforeend", this._navigationMonthEl);
+		div.insertAdjacentElement("beforeend", right);
+
+		return div;
+	}
+
+	_drawBody() {
 		let bodyDiv = document.createElement("div");
 		let weekDiv = null;
 
 		bodyDiv.classList.add("body");
 
-		let firstDate = this._getFirstDayOfMonth(month, year);
+		let firstDate = this._getFirstDayOfMonth();
 		let firstDayOfWeek = firstDate.getDay();
-		let lastDate = this._getLastDayOfMonth(month, year);
+		let lastDate = this._getLastDayOfMonth();
 		let lastDay = lastDate.getDate();
 
 		let started = false;
@@ -150,10 +279,15 @@ export class BabyCalendar extends HTMLElement {
 		return bodyDiv;
 	}
 
-	_drawBody() {
-		this._body = this._renderBody(this._month, this._year);
-		this._body.id = this._bodyID;
-		document.getElementById(this._bodyID).replaceWith(this._body);
+	_updateNavigation() {
+		let t = `${this._months[this._month]} - ${this._year}`;
+		document.getElementById(this._navigationMonthID).innerText = t;
+	}
+
+	_updateBody() {
+		this._bodyEl = this._drawBody();
+		this._bodyEl.id = this._bodyElID;
+		document.getElementById(this._bodyElID).replaceWith(this._bodyEl);
 	}
 }
 
